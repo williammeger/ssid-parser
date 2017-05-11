42ab531b5c0839a2111258173e26b5a27d307a0f
William Meger <wmeger14@gmail.com>
2017-05-11 12:15:07 -0400

    clean up

--- buildString.py
+++ buildString.py
@@ -3,6 +3,5 @@ prefix = 'dsc.discovery.com/tv-shows'
 with open('source.txt', 'r') as src:
     with open('output.txt', 'w') as dest:
        for line in src:
-#           dest.write('%s%s\n' % (prefix, line.rstrip('\n')))
-           dest.write('{}{}\n'.format(prefix, line.rstrip('\n')))
+           dest.write('{0}{1}\n'.format(prefix, line.rstrip('\n')))
            
